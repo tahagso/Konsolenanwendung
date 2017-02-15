f5dd8f12df7927c541a0e51008ee0b3f605b7e93
tahagso <iah51ucar@gso-koeln.de>
2017-02-15 08:51:19 +0100

    test multi

---
+++ Rechner/Rechner/Calculations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rechner
+{
+    class Calculations
+    {
+        public int Calculate(int parametera, int parameterb)
+        {
+            return parametera * parameterb;
+
+        }
+    }
+}
